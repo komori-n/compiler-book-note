cb1b1fc45c407f456033f3d14fcbb21eae866932
komori-n <ikamat.kmr@gmail.com>
2021-03-07 16:33:05 +0900

    feat: parse expression with nom

--- src/main.rs
+++ src/main.rs
@@ -1,154 +1,11 @@
-use anyhow::{anyhow, Context, Result};
-use std::iter::Peekable;
-use std::rc::Rc;
-use std::cell::RefCell;
+mod token;
 
+use anyhow::{Result, Context};
+use crate::token::Expr;
 use clap::{
     crate_authors, crate_description, crate_name, crate_version,
     Arg, App,
 };
-
-#[derive(Debug)]
-struct TokenList {
-    head: TokenLink,
-    tail: TokenLink,
-}
-
-type TokenLink = Option<Rc<RefCell<Token>>>;
-
-#[derive(Debug)]
-struct Token {
-    next: TokenLink,
-    content: TokenContent,
-}
-
-#[derive(Debug)]
-enum TokenContent {
-    Symbol(Symbol),
-    Number(i32),
-}
-
-#[derive(Debug, Clone, Copy)]
-enum Symbol {
-    Add,
-    Sub,
-}
-
-impl Symbol {
-    fn parse(c: char) -> Option<Symbol> {
-        match c {
-            '+' => Some(Symbol::Add),
-            '-' => Some(Symbol::Sub),
-            _ => None
-        }
-    }
-}
-
-impl TokenList {
-    fn new() -> Self {
-        Self { head: None, tail: None }
-    }
-
-    fn push(&mut self, content: TokenContent) {
-        let new_token = Token::new(content);
-        match self.tail.take() {
-            Some(tail) => {
-                tail.borrow_mut().next = Some(new_token.clone());
-                self.tail = Some(new_token);
-            },
-            None => {
-                self.head = Some(new_token.clone());
-                self.tail = Some(new_token);
-            }
-        }
-    }
-}
-
-
-impl Token {
-    fn new(content: TokenContent) -> Rc<RefCell<Self>> {
-        Rc::new(RefCell::new(Token { next: None, content }))
-    }
-}
-
-impl TokenList {
-    fn parse(expr: &str) -> Result<TokenList> {
-        let mut it = expr.chars().peekable();
-        let mut list = TokenList::new();
-
-        while let Some(&c) = it.peek() {
-            match c {
-                c if c.is_whitespace() => { it.next(); },
-                c if c.is_ascii_digit() => {
-                    let number = get_number(&mut it)?;
-                    list.push(TokenContent::Number(number));
-                },
-                c if Symbol::parse(c).is_some() => {
-                    let symbol = Symbol::parse(c).unwrap();
-                    list.push(TokenContent::Symbol(symbol));
-                    it.next();
-                }
-                _ => {
-                    return Err(anyhow!("unexpected symbol"));
-                }
-            }
-        }
-
-        Ok(list)
-    }
-
-    fn compile(&self) -> Result<()> {
-        println!(".intel_syntax noprefix");
-        println!(".global main");
-        println!("main:");
-
-        let head = self.head.as_ref()
-            .with_context(|| "parse result is null")?;
-
-        if let TokenContent::Number(number) = head.borrow().content {
-            println!("    mov rax, {}", number);
-        } else {
-            return Err(anyhow!("first token is not number"));
-        }
-
-        let mut prev_symbol: Option<Symbol> = None;
-        let mut token_link = head.borrow().next.clone();
-        while let Some(token) = token_link {
-            match &token.borrow().content {
-                TokenContent::Number(number) => {
-                    match prev_symbol {
-                        Some(Symbol::Add) => println!("    add rax, {}", number),
-                        Some(Symbol::Sub) => println!("    sub rax, {}", number),
-                        None => return Err(anyhow!("unexpected hoge")),
-                    }
-                    prev_symbol = None;
-                },
-                TokenContent::Symbol(symbol) => {
-                    prev_symbol = Some(*symbol);
-                },
-            }
-            token_link = token.borrow().next.clone();
-        }
-
-        println!("    ret");
-
-        Ok(())
-    }
-}
-
-fn get_number<T: Iterator<Item=char>>(it: &mut Peekable<T>) -> Result<i32> {
-    let mut number = 0;
-
-    if !it.peek().with_context(|| "number is expected")?.is_ascii_digit() {
-        return Err(anyhow!("number is expected"));
-    }
-
-    while let Some(Ok(digit)) = it.peek().map(|c| c.to_string().parse::<i32>()) {
-        number = number * 10 + digit;
-        it.next();
-    }
-    Ok(number)
-}
 
 fn main() -> Result<()> {
     let matches = App::new(crate_name!())
@@ -162,8 +19,10 @@ fn main() -> Result<()> {
     let expr = matches.value_of("expr")
         .with_context(|| "not found")?;
 
-    let token_list = TokenList::parse(expr)?;
-    token_list.compile()?;
+    let expr = Expr::parse(expr)
+        .expect("parse error");
+
+    expr.compile();
 
     Ok(())
 }
---
+++ src/token/token.rs
@@ -0,0 +1,263 @@
+use anyhow::{anyhow, Context, Result};
+use nom::{IResult, Offset, branch::alt, character::complete::char, character::complete::{digit1, multispace0}, combinator::{map, map_res, opt}, delimited, sequence::{delimited, tuple}};
+use nom::{Err, error::Error};
+use std::{hint::unreachable_unchecked, iter::Peekable};
+use std::rc::Rc;
+use std::cell::RefCell;
+
+
+// expr    = mul ("+" mul | "-" mul)*
+// mul     = primary ("*" primary | "/" primary)*
+// primary = num | "("expr")"
+
+// <expr>       = <mul> [('+' | '-') <expr>]
+// <mul>        = <primary> [('*' | '/') <mul>]
+// <primary>    = <num> | <paren_expr>
+// <paren_expr> = '(' <expr> ')'
+// <num>        = [0-9]+
+
+pub type Num = i64;
+
+#[derive(Debug, PartialEq)]
+pub enum Expr {
+    Num(Num),
+    BinaryOperation(BinaryOperation),
+}
+
+#[derive(Debug, PartialEq)]
+pub struct BinaryOperation {
+    pub op: OperatorKind,
+    pub left: Box<Expr>,
+    pub right: Box<Expr>
+}
+
+#[derive(Debug, PartialEq)]
+pub enum OperatorKind {
+    Add,
+    Sub,
+    Mul,
+    Div
+}
+
+impl Expr {
+    pub fn parse(s: &str) -> Result<Expr, Err<Error<&str>>> {
+        expr_parser(s)
+            .map(|(_, expr)| expr)
+    }
+
+    pub fn compile(&self) {
+        println!(".intel_syntax noprefix");
+        println!(".global main");
+        println!("main:");
+
+        self.generate();
+
+        println!("    pop rax");
+        println!("    ret");
+    }
+
+    fn generate(&self) {
+        match self {
+            Expr::Num(num) => {
+                println!("    push {}", num);
+            },
+            Expr::BinaryOperation(bin_op) => {
+                bin_op.left.generate();
+                bin_op.right.generate();
+
+                println!("    pop rdi");
+                println!("    pop rax");
+
+                match bin_op.op {
+                    OperatorKind::Add => println!("    add rax, rdi"),
+                    OperatorKind::Sub => println!("    sub rax, rdi"),
+                    OperatorKind::Mul => println!("    imul rax, rdi"),
+                    OperatorKind::Div => {
+                        println!("    cqo");
+                        println!("    idiv rax, rdi");
+                    },
+                }
+
+                println!("    push rax");
+            }
+        }
+    }
+}
+
+fn offset_op_parser(s: &str) -> IResult<&str, OperatorKind> {
+    alt((
+        map(
+            char('+'),
+            |_| OperatorKind::Add,
+        ),
+        map(
+            char('-'),
+            |_| OperatorKind::Sub,
+        )
+    ))(s)
+}
+
+fn scale_op_parser(s: &str) -> IResult<&str, OperatorKind> {
+    alt((
+        map(
+            char('*'),
+            |_| OperatorKind::Mul,
+        ),
+        map(
+            char('/'),
+            |_| OperatorKind::Div,
+        )
+    ))(s)
+}
+
+pub fn expr_parser(s: &str) -> IResult<&str, Expr> {
+    map(
+        tuple((
+            ws(mul_parser),
+            opt(tuple((
+                ws(offset_op_parser),
+                ws(expr_parser)
+            )))
+        )),
+        |(left, opt)| {
+            if let Some((op, right)) = opt {
+                Expr::BinaryOperation(BinaryOperation {
+                    op,
+                    left: Box::new(left),
+                    right: Box::new(right)
+                })
+            } else {
+                left
+            }
+        }
+    )(s)
+}
+
+fn mul_parser(s: &str) -> IResult<&str, Expr> {
+    map(
+        tuple((
+            ws(primary_parser),
+            opt(tuple((
+                ws(scale_op_parser),
+                ws(primary_parser)
+            )))
+        )),
+        |(left, opt)| {
+            if let Some((op, right)) = opt {
+                Expr::BinaryOperation(BinaryOperation {
+                    op,
+                    left: Box::new(left),
+                    right: Box::new(right)
+                })
+            } else {
+                left
+            }
+        }
+    )(s)
+}
+
+fn primary_parser(s: &str) -> IResult<&str, Expr> {
+
+    alt((
+        map(
+            ws(num_parser),
+            |num| Expr::Num(num)
+        ),
+        ws(paren_expr_parser)
+    ))(s)
+}
+
+fn paren_expr_parser(s: &str) -> IResult<&str, Expr> {
+    let (no_used, _) = char('(')(s)?;
+    let (no_used, expr) = expr_parser(no_used)?;
+    let (no_used, _) = char(')')(no_used)?;
+
+    Ok((no_used, expr))
+}
+
+fn num_parser(s: &str) -> IResult<&str, Num> {
+    map_res(
+        digit1,
+        |s: &str| s.parse::<Num>()
+    )(s)
+}
+
+fn ws<'a, F: 'a, O>(inner: F) -> impl FnMut(&'a str) -> IResult<&'a str, O>
+    where
+    F: Fn(&'a str) -> IResult<&'a str, O>,
+{
+    delimited(
+        multispace0,
+        inner,
+        multispace0
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fmt::Binary;
+
+    use super::*;
+
+    #[test]
+    fn offset_op_parser_test() {
+        assert_eq!(offset_op_parser("+abc").unwrap(), ("abc", OperatorKind::Add));
+        assert_eq!(offset_op_parser("-abc").unwrap(), ("abc", OperatorKind::Sub));
+        assert!(offset_op_parser("*abc").is_err());
+        assert!(offset_op_parser("/abc").is_err());
+    }
+
+    #[test]
+    fn scale_op_parser_test() {
+        assert!(scale_op_parser("+abc").is_err());
+        assert!(scale_op_parser("-abc").is_err());
+        assert_eq!(scale_op_parser("*abc").unwrap(), ("abc", OperatorKind::Mul));
+        assert_eq!(scale_op_parser("/abc").unwrap(), ("abc", OperatorKind::Div));
+    }
+
+    #[test]
+    fn expr_parser_test() {
+        assert_eq!(expr_parser("334abc").unwrap(), ("abc", Expr::Num(334)));
+        assert_eq!(expr_parser("334+264*227").unwrap(), ("",
+            Expr::BinaryOperation(BinaryOperation {
+                op: OperatorKind::Add,
+                left: Box::new(Expr::Num(334)),
+                right: Box::new(Expr::BinaryOperation(BinaryOperation {
+                    op: OperatorKind::Mul,
+                    left: Box::new(Expr::Num(264)),
+                    right: Box::new(Expr::Num(227))
+                }))
+            })));
+    }
+
+    #[test]
+    fn primary_parser_test() {
+        assert_eq!(primary_parser("334abc").unwrap(), ("abc", Expr::Num(334)));
+        assert_eq!(primary_parser("(264)abc").unwrap(), ("abc", Expr::Num(264)));
+        assert!(primary_parser("(227").is_err());
+        assert!(primary_parser("(def)").is_err());
+    }
+
+    #[test]
+    fn mul_parser_test() {
+        assert_eq!(mul_parser("334abc").unwrap(), ("abc", Expr::Num(334)));
+        assert_eq!(mul_parser("(334)abc").unwrap(), ("abc", Expr::Num(334)));
+        assert_eq!(mul_parser("334*264").unwrap(), ("", Expr::BinaryOperation(BinaryOperation {
+            op: OperatorKind::Mul,
+            left: Box::new(Expr::Num(334)),
+            right: Box::new(Expr::Num(264))
+        })));
+    }
+
+    #[test]
+    fn paren_expr_parser_test() {
+        assert_eq!(paren_expr_parser("(334)abc").unwrap(), ("abc", Expr::Num(334)));
+        assert!(paren_expr_parser("(abc)").is_err());
+    }
+
+    #[test]
+    fn num_parser_test() {
+        assert_eq!(num_parser("334abc").unwrap(), ("abc", 334));
+        assert!(num_parser("abc").is_err());
+    }
+}
